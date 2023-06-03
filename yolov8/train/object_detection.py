72e8e936cd84e1d82763396dcfc4276f32f06d89
narugo1992 <narugo@126.com>
2023-06-03 21:50:29 +0800

    dev(narugo): fix resume

--- yolov8/train/object_detection.py
+++ yolov8/train/object_detection.py
@@ -10,13 +10,8 @@ def train_object_detection(workdir: str, train_cfg: str, level: str = 's',
 
     # Load a pretrained YOLO model (recommended for training)
     previous_pt = os.path.join(workdir, 'weights', 'last.pt')
-    if not os.path.exists(previous_pt):
-        logging.info(f'Initialize model from yolov8{level}.pt')
-        model, resume = YOLO(f'yolov8{level}.pt'), True
-    else:
-        logging.info(f'Resume previous ckpt from {previous_pt!r}')
-        logging.warn(f'Resumed model cannot be published to roboflow!!!')
-        model, resume = YOLO(previous_pt), True
+    model = YOLO(f'yolov8{level}.pt')
+    resume = os.path.exists(previous_pt)
     workdir = os.path.abspath(workdir)
 
     # Train the model using the 'coco128.yaml' dataset for 3 epochs
