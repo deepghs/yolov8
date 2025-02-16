aa31179f5ad58a5122433bf7a861a6ebae0b0505
narugo1992 <narugo992@gmail.com>
2025-02-16 17:40:34 +0800

    dev(narugo): show result

--- yolov8/utils/f1plot.py
+++ yolov8/utils/f1plot.py
@@ -9,7 +9,7 @@ from imgutils.ocr import ocr
 def get_f1_and_threshold_from_image(image: ImageTyping) -> Tuple[Optional[float], Optional[float]]:
     threshold, max_f1_score = None, None
     for _, label, _ in ocr(image):
-        logging.info('Scanning OCR result: {label!s}')
+        logging.info(f'Scanning OCR result: {label!s}')
         matching = re.fullmatch(r'^\s*a+l{2,}\s+c+l+a+s+s+(e+s+)?\s+(?P<f1>\d+(\.+\d+)?)\s+'
                                 r'a+t+\s+(?P<threshold>\d+(\.+\d+)?)\s*$', label)
         if matching:
